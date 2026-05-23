54baf1c4a2e3754f168cb15d46ef2ac0fa3fbf9d
awaboi <KIW1boxedu@gmail.com>
2026-05-23 16:07:18 -0400

    fixed bug where shulkers didn't show up in ChestESP and ChestESP blocks were showing up in BaseESP

--- src/client/java/com/tsc/client/util/render/EspUtils.java
+++ src/client/java/com/tsc/client/util/render/EspUtils.java
@@ -11,16 +11,26 @@ public class EspUtils {
             Blocks.TRAPPED_CHEST,
             Blocks.ENDER_CHEST,
             Blocks.BARREL,
-            Blocks.SHULKER_BOX
-            // add more blocks here
+            Blocks.SHULKER_BOX,
+            Blocks.WHITE_SHULKER_BOX,
+            Blocks.ORANGE_SHULKER_BOX,
+            Blocks.MAGENTA_SHULKER_BOX,
+            Blocks.LIGHT_BLUE_SHULKER_BOX,
+            Blocks.YELLOW_SHULKER_BOX,
+            Blocks.LIME_SHULKER_BOX,
+            Blocks.PINK_SHULKER_BOX,
+            Blocks.GRAY_SHULKER_BOX,
+            Blocks.LIGHT_GRAY_SHULKER_BOX,
+            Blocks.CYAN_SHULKER_BOX,
+            Blocks.PURPLE_SHULKER_BOX,
+            Blocks.BLUE_SHULKER_BOX,
+            Blocks.BROWN_SHULKER_BOX,
+            Blocks.GREEN_SHULKER_BOX,
+            Blocks.RED_SHULKER_BOX,
+            Blocks.BLACK_SHULKER_BOX
     );
 
     public static final Set<Block> BASE_ESP_BLOCKS = Set.of(
-            Blocks.CHEST,
-            Blocks.TRAPPED_CHEST,
-            Blocks.ENDER_CHEST,
-            Blocks.BARREL,
-            Blocks.SHULKER_BOX,
             Blocks.ENCHANTING_TABLE,
             Blocks.CAKE,
             Blocks.CRAFTING_TABLE,
